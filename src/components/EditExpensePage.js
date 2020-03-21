6a65584696738941e6dd885911b7dcdf375040b5
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-03-22 01:34:54 +0530

    commit

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component 
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        
+            <div>
+               Editing expense with id {props.match.params.id}
+            </div>
+    );
+};
 
 export default EditExpensePage;
