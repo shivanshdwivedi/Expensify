3c128e5ceda24ce5442f51f2606c53d20d903e7d
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-03-24 00:35:31 +0530

    commit

---
+++ src/playground/destructuring.js
@@ -0,0 +1,15 @@
+// jshint esversion: 6
+
+
+const book = {
+
+    title: 'Ego',
+    author: 'Josh Vango',
+    publisher: {
+        name: 'Angelaa Kno'
+    }
+}
+
+const {name : publisherName = 'Itself'} = book.publisher;
+
+console.log(publisherName);
