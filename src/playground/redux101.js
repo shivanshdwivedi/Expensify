4422315a14b2c8fbdb849f6e26e4ca9bb420e225
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-03-23 23:52:10 +0530

    commit

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,6 +1,60 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = {count: 0}) => {
-    return state;
+const store = createStore((state = {count: 0} , action) => {
+     
+    switch(action.type){
+        case'INCREMENT':
+        return{
+            count: state.count + 1
+        };    
+
+        case'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1 ;
+        return{
+            count: state.count - decrementBy
+        };
+
+        case'RESET':
+        return{
+            count: 0
+        };
+
+        case 'SET':
+            return{
+                count: action.count
+            }
+
+        default:
+            return state;
+    }
+
 }); 
+
+const subscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy : 10
+});
+
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
+
 console.log(store.getState());
