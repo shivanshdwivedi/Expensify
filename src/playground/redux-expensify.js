d90f53bb52d0d580afa9a9e98fce06f7881a3618
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-03-26 01:11:46 +0530

    commit

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -3,6 +3,37 @@
 
 import {createStore , combineReducers } from 'redux';
 
+const expensesReducersDefaultState = [];
+const expensesReducers = (state = expensesReducersDefaultState , action) => {
+   switch(action.type){
+       default:
+           return state;
+   }
+};
+
+const filterReducersDefaultState = {
+    text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+};
+const filterReducers = (state = filterReducersDefaultState , action) => {
+   switch(action.type){
+       default:
+           return state;
+   }
+};
+
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducers,
+        filters: filterReducers
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'hcidbdbc',
