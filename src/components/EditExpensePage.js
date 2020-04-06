e8cef322cdcc1dba5091b46832ccf538710b102f
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-07 02:01:00 +0530

    commit

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
 
 const EditExpensePage = (props) => {
     console.log(props);
     return (
         
             <div>
-               Editing expense with id {props.match.params.id}
+               <ExpenseForm 
+                  expense = {props.expense}
+                   onSubmit = {(expense) => {
+                       console.log('updated' , expense);
+                   }}
+               />
             </div>
     );
 };
 
-export default EditExpensePage;
+
+ const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expense.find((expense) => expense.id === props.match.params.id)
+    };
+};
+export default connect(mapStateToProps)(EditExpensePage);
