0c775ff6d31706a0fa4c7cc10b33dbc83e85c2c3
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-11 20:36:05 +0530

    commit

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -20,3 +20,35 @@ test('' , () => {
         }
     });
 });
+
+test('' , () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt:1000,
+        note: 'This was last months rent'
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('' , () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+                description: '',
+                 amount: 0,
+               createdAt:0,
+                 note: ''
+            
+        }
+    });
+});
