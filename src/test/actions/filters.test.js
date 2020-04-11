71b840fcb3d90856151ce6c6426a628cd9e4e300
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-12 01:14:09 +0530

    commit

--- src/test/actions/filters.test.js
+++ src/test/actions/filters.test.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import {setStartDate , setEndDate } from '../../actions/filters';
+import {setStartDate , setEndDate, setTextFilter ,sortByDate , sortByAmount } from '../../actions/filters';
 
 test('' , () => {
     const action = setStartDate(moment(0));
@@ -16,3 +16,38 @@ test('' , () => {
         endDate: moment(0)
     });
 });
+
+
+test('' , () => {
+    const action = setTextFilter('Something in');
+   expect(action).toEqual({
+       type:'SET_TEXT_FILTER',
+       text: 'Something in'
+   });
+});
+
+test('' , () => {
+    const action = setTextFilter();
+   expect(action).toEqual({
+       type:'SET_TEXT_FILTER',
+       text: ''
+   });
+});
+
+test('' , () => {
+    const action = sortByAmount();
+   expect(action).toEqual({
+     type: 'SORT_BY_AMOUNT'
+   });
+});
+
+test('' , () => {
+    const action = sortByDate();
+   expect(action).toEqual({
+     type: 'SORT_BY_DATE'
+   });
+});
+
+
+
+
