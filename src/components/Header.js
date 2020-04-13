3e846b996885f029d0d3bf39d27477b68da6c551
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-14 01:34:13 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
         </h1>
         <NavLink to = "/" activeClassName = "is-active" exact = {true}>Dashboard</NavLink>
         <NavLink to = "/create" activeClassName = "is-active">Create Expense</NavLink>
-        <NavLink to = "/help" activeClassName = "is-active">Help</NavLink>
+        {/* <NavLink to = "/help" activeClassName = "is-active">Help</NavLink> */}
         
     </header>
    
---
+++ src/test/components/header.test.js
@@ -0,0 +1,10 @@
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import React from 'react';
+import Header from '../../components/Header';
+
+test('' , () => {
+    const renderer =new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    console.log(renderer.getRenderOutput());
+});
