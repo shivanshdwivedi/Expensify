4938faec341773cdf6b2c9a521bcb33fcf3b5298
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-16 01:29:46 +0530

    commit

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -21,3 +21,41 @@ test('' , () => {
     expect(wrapper.state('error').length).toBeGreaterThan(0);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('' , () => {
+    const value = 'New Description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change' , {
+        target: {value}
+    });
+    expect(wrapper.state('description')).toBe(value);
+});
+
+test('' , () => {
+    const value = 'New Note value';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').at(0).simulate('change' , {
+        target: {value}
+    });
+    expect(wrapper.state('note')).toBe(value);
+});
+
+test('' , () => {
+    const value = '23.5';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change' , {
+        target: {value}
+    });
+    expect(wrapper.state('amount')).toBe(value);
+});
+
+test('' , () => {
+    const value = '12.122';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change' , {
+        target: {value}
+    });
+    expect(wrapper.state('amount')).toBe('');
+});
+
+
