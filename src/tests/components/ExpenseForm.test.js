272179e1836655f60651be6b500c2096ca32a1cb
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-17 01:03:37 +0530

    commit

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import moment from'moment';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -58,4 +59,24 @@ test('' , () => {
     expect(wrapper.state('amount')).toBe('');
 });
 
+test('' , () => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense ={expenses[0]} onSubmit = {onSubmitSpy}/>);
+    wrapper.find('form').simulate('submit' , { 
+    preventDefault: () => {}
+    });
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        description: expenses[0].description,
+        amount: expenses[0].amount,
+        note: expenses[0].note,
+        createdAt: expenses[0].createdAt
+    });
+});
 
+test('' , () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toEqual(now);
+});
