8df0c921a6a2734a7a7d181f635cfed9f3e9e1c3
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-19 01:31:12 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
+// jshint esversion : 6
+
 const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname , '..' , 'public');
 app.use(express.static(publicPath));
+const port = process.env.PORT || 3000;
 
 app.get('*' , (req , res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000 , () => {
+app.listen(port , () => {
     console.log('server is up on 3000 port!');
 });
