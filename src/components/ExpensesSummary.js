017243ffe3f0aec5903f60c3e63786274776220d
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-04-21 23:24:48 +0530

    commit

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import numeral from 'numeral';
-
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 export const ExpensesSummary = ({expenseCount , expensesTotal}) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal).format('Rs0,0.00')
@@ -11,3 +12,13 @@ export const ExpensesSummary = ({expenseCount , expensesTotal}) => {
         </div>
     )
 };
+
+ const mapStateToProps = (state) => {
+   const visibleExpenses = selectExpenses(state.expenses , state.filters);
+   return{
+       expenseCount: visibleExpenses.length,
+       expensesTotal : selectExpensesTotal(visibleExpenses)
+   };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary)
