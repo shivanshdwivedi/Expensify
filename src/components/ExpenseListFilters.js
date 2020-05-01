4a8d88ab53bcad5faed6654d067b34cda4e2b56e
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-05-02 02:16:04 +0530

    commit

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -29,20 +29,26 @@ export class ExpenseListFilters extends React.Component {
   };
   render() {
     return (
-      <div>
-        <input
+      <div className = "content-container">
+      <div className = "input-group">
+      <div className = "input-group__item">
+      <input
           type="text"
           value={this.props.filters.text}
           onChange={this.onTextChange}
         />
-        <select
+      </div>
+      <div className = "input-group__item">
+      <select
           value={this.props.filters.sortBy}
           onChange={this.onSortChange}
         >
           <option value="date">Date</option>
           <option value="amount">Amount</option>
         </select>
-        <DateRangePicker
+      </div>
+      <div className = "input-group__item">
+      <DateRangePicker
           startDate={this.props.filters.startDate}
           endDate={this.props.filters.endDate}
           onDatesChange={this.onDatesChange}
@@ -52,6 +58,8 @@ export class ExpenseListFilters extends React.Component {
           numberOfMonths={1}
           isOutsideRange={() => false}
         />
+      </div>
+      </div>
       </div>
     );
   }
