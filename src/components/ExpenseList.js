63d525ac5e8554a69e24ccf95cd77461089ef74f
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-05-03 01:53:31 +0530

    commit

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,16 +6,27 @@ import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 export const ExpenseList = (props) => (
-    <div>
-        {
+    <div className = "content-container">
+    <div className = "list-header"> 
+      <div className = "show-for-mobile">Expenses</div>
+      <div className = "show-for-desktop">Expense</div>
+      <div className = "show-for-desktop">Amount(in ₹)</div>
+    </div>
+       <div className = "list-body">
+       {
             props.expenses.length ===0 ? (
-                <p>No expenses</p>
+               <div className ="list-item--message"> 
+               <span> 
+                No expenses
+               </span>
+               </div>
             ) : (props.expenses.map((expense) => {
                return <ExpenseListItem key = {expense.id} {...expense}/>;
            })
 
             )
-        }
+        } 
+       </div>
            
     </div>
 );
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,16 +6,16 @@ import numeral from 'numeral';
 import moment from 'moment';
 
 const ExpenseListItem = ({description , amount , createdAt ,  id }) => (
+  
+    <Link className = "list-item" to ={`/edit/${id}`}>   
     <div>
-    <Link to ={`/edit/${id}`}>   
-    <h3> {description} </h3>
-    </Link>
-        <p>
-        {numeral(amount).format('Rs0,0.00')} 
-        - 
-        {moment(createdAt).format('MMMM Do , YYYY')}
-        </p>
+    <h3 className = "list-item__title"> {description} </h3>
+    <span className = "list-item__sub-title"> {moment(createdAt).format('MMMM Do , YYYY')}</span>
     </div>
+    <h3 className ="list-item__data">{numeral(amount).format('0,0.00')} </h3>
+    </Link>
+       
+    
 );
 
 export default ExpenseListItem;
